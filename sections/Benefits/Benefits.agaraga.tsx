86a8378565e0339010970d9739bf36e8c98812ab
Crisnzx <cristovaon13@gmail.com>
2023-05-20 14:41:28 -0300

    chore: continue implementation

--- sections/Benefits/Benefits.agaraga.tsx
+++ sections/Benefits/Benefits.agaraga.tsx
@@ -13,24 +13,30 @@ export interface BenefitProps {
 
 function Benefit({ title, description }: BenefitProps) {
   return (
-    <div className="">
-      <Icon id="Deco" /> <h3>{title}</h3> <p>{description}</p>
+    <div className="bg-blue-400 flex flex-col items-center p-6 mx-4 lg:w-72">
+      <Icon id="Deco" width={30} height={30} />
+      <h3 className=" text-base-100 text-xl mb-2">{title}</h3>
+      <p className="text-base-100 text-sm">{description}</p>
     </div>
   );
 }
 
 function Benefits({ title, description, benefits }: Props) {
   return (
-    <section className="">
-      <h2 className="text-base-50 text-center text-4xl">{title}</h2>
-      <p className="text-center text-2xl">{description}</p>
-      {benefits?.map((benefit) => (
-        <Benefit
-          key={benefit.title}
-          title={benefit.title}
-          description={benefit.description}
-        />
-      ))}
+    <section className="py-10 px-12 bg-red-400">
+      <h2 className="text-base-content text-center text-4xl mb-2">{title}</h2>
+      <p className="text-neutral text-center text-2xl mb-10">{description}</p>
+      <div className="flex justify-center">
+        <div className="flex mx-auto bg-blue-100">
+          {benefits?.map((benefit) => (
+            <Benefit
+              key={benefit.title}
+              title={benefit.title}
+              description={benefit.description}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
